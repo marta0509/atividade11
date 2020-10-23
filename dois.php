9dd74cf3fccfc6b13ac5489a5bc632044752bd4f
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-23 12:23:26 +0100

    Update dois.php, tres.php, and um.php

---
+++ dois.php
@@ -0,0 +1,5 @@
+<?php
+$frase = "Estou a aprender PHP na secundária.";
+
+echo str_replace("secundária", "escola", "Estou a aprender PHP na secundária.");
+?>
---
+++ um.php
@@ -0,0 +1,9 @@
+<?php
+$pais = 'Portugal';
+
+echo "O 4º e o 5º caracter  são: " .substr($pais, 4,2);
+echo "<br>";
+echo "A quantidade de caracteres é: " .strlen($pais);
+echo "<br>";
+echo strtoupper($pais);
+?>
